c5d0d491fff7648f4e972dacf0a90276753d7a6f
yunus emre fersiz <yemref0716@hotmail.com>
2024-05-07 14:31:20 +0300

    Add individual lang file for i18N with async loading feature for English

---
+++ lang/en-EN.ts
@@ -0,0 +1,13 @@
+export default defineI18nLocale(async locale => {
+
+    return {
+        home: "Home",
+        about: "About",
+        services: "Services",
+        blog: "Blog",
+        contact: "Contact",
+        advertisement: "Advertisements",
+        callNow: "Call Now",
+        getAddress: "Get Address"
+      }
+})
