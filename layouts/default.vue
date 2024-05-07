ded4a20eeed178f490fab537bdc3593dfce1ee19
yunus emre fersiz <yemref0716@hotmail.com>
2024-05-07 14:33:49 +0300

    Add default layout structure of the website

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <TheHeader />
+        <slot></slot>
+    <TheFooter/>
+</template>
+
+<script setup>
+import TheHeader from '~/components/layouts/TheHeader.vue';
+import TheFooter from '~/components/layouts/TheFooter.vue';
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
