fade46c223be09e9c216ac04430b4b33155cacfa
yunus emre fersiz <yemref0716@hotmail.com>
2024-05-07 14:30:48 +0300

    Add Tailwind Css , Nuxt Image, İ18n, Google Fonts and Nuxt Icon libraries to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,55 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+
+  components: {
+    dirs: [
+      "~/components",
+      "~/components/buttons",
+      "~/components/layouts",
+    ],
+  },
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  modules: ["@nuxtjs/i18n", '@nuxtjs/google-fonts', "@nuxt/image",'nuxt-icon'],
+
+  i18n: {
+    defaultLocale: 'tr',
+    lazy: false,
+    langDir: 'lang',
+    locales: [
+        {
+            code: 'tr',
+            name: 'Türkçe',
+            file: 'tr-TR.json'
+        },
+        {
+            code: 'en',
+            name: 'English',
+            file: 'en-EN.ts'
+        },
+    ],
+  },
+
+  googleFonts: {
+    display: 'swap',
+    families: {
+      Spartan: [300,400,500,600,700,800,900],
+      Poppins: {
+        wght: [100,300,400,500,600],
+        ital: [100]
+      },
+    }
+  },
+
+  image: {
+    format:['webp', 'avif', 'jpeg', 'jpg', 'png']
+  }
 })
