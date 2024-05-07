cca9c2f0989cb5358ae5a0ec865f52dde7c2d0c9
yunus emre fersiz <yemref0716@hotmail.com>
2024-05-07 14:32:21 +0300

    Add Container UI component

---
+++ components/UI/Container.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container mx-auto px-4 md:px-6 lg:px-12 xl:px-24">
+        <slot />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
