9ff047be17d460f02be0151a400f75706609422f
yunus emre fersiz <yemref0716@hotmail.com>
2024-05-14 01:12:24 +0300

    Add Bread Crumb 1 component and Bread Crumb 2 Component

---
+++ components/UI/BreadCrumb.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="bread-bg my-6 group">
+    <div class="flex flex-col justify-center items-center gap-4">
+      <h1 class="text-center text-3xl xl:text-4xl font-spartan font-semibold capitalize">İletişim</h1>
+      <p class="mt-2 font-poppins">
+        <nuxt-link :to="localePath('/')" title="Anasayfa">Anasayfa </nuxt-link>
+        >
+        <span class="text-body"> İletişim </span>
+      </p>
+    </div>
+    <NuxtImg
+     class="absolute bottom-5 left-10 group-hover:animate-bounce"
+      src="/images/shapes/shape-1.png"
+      loading="lazy"
+      alt="shape1"
+      sizes="100px sm:200px md:300px"
+    />
+    <NuxtImg
+     class="absolute top-10 right-10 group-hover:animate-bounce"
+      src="/images/shapes/shape-2.png"
+      loading="lazy"
+      alt="shape1"
+      sizes="100px sm:200px md:300px"
+    />
+  </div>
+</template>
+
+<script setup>
+const localePath = useLocalePath();
+</script>
+
+<style lang="scss" scoped>
+.bread-bg {
+  background-color: #f5f9fa;
+  padding: 95px 0 85px;
+  position: relative;
+  z-index: 1;
+  overflow: hidden;
+}
+
+
+</style>
---
+++ components/UI/BreadCrumb2.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="bread-bg my-6 group text-tlight">
+    <Container>
+      <div class="flex flex-col justify-center items-start gap-4">
+        <h2
+          class="text-center text-2xl xl:text-3xl font-poppins font-semibold capitalize"
+        >
+          Hanem Harita
+        </h2>
+        <h1
+          class="text-center text-3xl xl:text-6xl font-spartan font-semibold mt-3 uppercase"
+        >
+          İLETİŞİM
+        </h1>
+        <p class="lg:w-1/2">
+          Hizmetlerimiz hakkında detaylı bilgi almak için bizimle iletişime
+          geçebilir, blog kısmı için öneri ve isteklerde bulunabilirsiniz.
+        </p>
+      </div>
+    </Container>
+  </div>
+</template>
+
+<script setup>
+import Container from "./Container.vue";
+
+const localePath = useLocalePath();
+</script>
+
+<style lang="scss" scoped>
+.bread-bg {
+  padding: 120px 0 100px;
+  position: relative;
+  z-index: 1;
+  overflow: hidden;
+  background-image: url(/images/hero/hero.jpg);
+}
+
+@media screen and (max-width: 991px) {
+  .bread-bg {
+    padding: 50px 0 50px;
+    position: relative;
+    z-index: 1;
+    overflow: hidden;
+    background-image: url(/images/hero/hero.jpg);
+  }
+}
+</style>
