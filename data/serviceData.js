6d1aa1bc56932a93d300eae068fe667ff953b73c
yunus emre fersiz <yemref0716@hotmail.com>
2024-05-15 03:16:25 +0300

    Add static services data

---
+++ data/serviceData.js
@@ -0,0 +1,48 @@
+export default [
+  {
+    title: "Tapu İfrazı Hizmetleri",
+    desc: "İmar Kanununa göre belediye ve mücavir alan sınırları içindeki taşınmazların ifrazından belediyeler sorumludur. Belediye encümeninin onayı ile ifraz kararı resen yani bağımsız olarak alınabilir.İfraz işlemleri, parselasyon nitelikli ve parselasyon nitelikli olmayan biçiminde ikiye ayrılır. Parselasyon nitelikli ifraz işlemi; bir parselin kamu yararı doğrultusunda okul, yol, ibadethane, park, yeşil alan gibi amaçlar için kamuya bırakılmasını kapsar. Parselasyon niteliği olmayan ifrazlar ise kamuya bırakılması söz konusu olmayan alanlar içerisinde gerçekleştirilir.",
+    img: "1.jpg",
+    documents: [
+      "Kimlik Fotokopisi",
+      "Tapu Nüshası",
+      "İmar durumu belgesi",
+      "Komşu tapu sahiplerinin muvafakat belgesi (parselasyon nitelikli ifrazlarda)",
+      "Belediye tarafından istenen diğer belgeler",
+    ],
+  },
+
+  {
+    title: "Kadastro Haritalama Hizmetleri",
+    desc: "Kadastro haritalama, arazi parsellerinin ve bunlara ait mülkiyet bilgilerinin detaylı haritalarını oluşturma işlemidir. Bu haritalar, tapu kayıtlarının güncellenmesi, vergi değerlendirmeleri, arazi planlaması ve mülkiyet anlaşmazlıklarının çözümü gibi çeşitli amaçlar için kullanılır.",
+    img: "2.jpg",
+    documents: [
+      "Tapu kaydı",
+      "Arazi sınırlarının belirlenmesi",
+      "Komşu parsellerin tapu bilgileri",
+      "Belediye tarafından istenen diğer belgeler"
+    ],
+  },
+
+  {
+    title: "Topografik Haritalama Hizmetleri",
+    desc: "Topografik haritalama, bir arazi yüzeyinin yükseklik, arazi şekli, bitki örtüsü ve su kütleleri gibi fiziksel özelliklerini gösteren haritalar oluşturma işlemidir. Bu haritalar, mühendislik ve inşaat planlaması, çevre planlaması ve yönetimi, doğal kaynak yönetimi ve rekreasyonel faaliyetler gibi çeşitli amaçlar için kullanılır.",
+    img: "1.jpg",
+    documents: [
+      "Haritalanacak alanın sınırları",
+      "Ölçek ve amaç",
+      "Belediye tarafından istenen diğer belgeler"
+    ],
+  },
+
+  {
+    title: "GIS (Coğrafi Bilgi Sistemleri) Hizmetleri",
+    desc: " Coğrafi Bilgi Sistemleri (GIS), coğrafi verileri entegre etme, depolama, analiz etme ve görselleştirme imkanı sunan bilgisayar sistemleridir. GIS hizmetleri, veri yönetimi ve analizi, haritalama ve görselleştirme, karar verme desteği gibi çeşitli faydalar sağlayabilir.",
+    img: "2.jpg",
+    documents: [
+      "Veri ve gereksinimler projeye göre değişir",
+    ],
+  },
+
+
+];
